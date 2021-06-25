12618ec0565b135f78a14721345b7debba8c03c0
zhangguoqing <zhangguoqing84@westlake.edu.cn>
2021-06-25 22:39:35 +0800

    update

--- AutoNormalizition.py
+++ AutoNormalizition.py
@@ -1,20 +1,17 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Feb  6 11:29:06 2020
+'''
+Author: zhangguoqing
+Date: 2020-02-06 11:27:32
+LastEditTime: 2021-06-25 22:35:48
+'''
 
-@author: zhangguoqing
-
-E-mail: zhangguoqing84@westlake.edu.cn
-
-
-"""
 
 def normalizition(data):
     min_value = data.min()
     max_value = data.max()
     ranges = max_value - min_value
     norm_data = zeros(shape(data))
-    m = data.shape[]
+    m = data.shape()
     norm_data = data - tile(min_value, (m, 1))
     norm_data = norm_data/tile(ranges, (m, 1))
     return norm_data, ranges, min_value
---
+++ README.md
@@ -0,0 +1,12 @@
+
+
+<!--
+ * @Author: zhangguoqing
+ * @Date: 2021-06-25 22:36:01
+ * @LastEditTime: 2021-06-25 22:39:11
+-->
+### This code repository is a collection of widgets designed to help with bioinformatics analysis.
+  
+  
+In order to be lightweight, complex parameters are not written, so some scripts need to be modified and used as required.   
+But that's okay, because the code is simple, so enjoy the refactoring.
